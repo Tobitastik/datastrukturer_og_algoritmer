1b83bddbdedcd5ff601b18bfd42383f1bd5d48a0
Tobias <144216903+Tobitastik@users.noreply.github.com>
2025-10-29 21:04:17 +0100

    set, insert, remove and clear

--- Datastruktur-portfolio/DynamicArray/dynamicarray/dynamicarray.js
+++ Datastruktur-portfolio/DynamicArray/dynamicarray/dynamicarray.js
@@ -46,4 +46,53 @@ export default class DynamicArray {
         }
         return this.#_arr.get(index);
     }
+
+    set(index, item){
+       if(index < 0 || index >= this.#_size){
+            throw new RangeError("Index out of bounds");
+        }
+        this.#_arr.set(index, item); 
+    }
+
+    insert(index, item){
+        if(index < 0 || index > this.#_size){
+            throw new RangeError("Index out of bounds");
+        }
+
+        if(this.#_size >= this.capacity()){
+            this.grow();
+        }
+
+        for(let i = this.#_size; i > index; i--){
+            this.#_arr.set(i, this.#_arr.get(i-1));
+        }
+
+        this.#_arr.set(index, item);
+        this.#_size++;
+    }
+
+    remove(index){
+        if(index < 0 || index >= this.#_size){
+            throw new RangeError("Index out of bounds")
+        }
+
+        const removedItem = this.#_arr.get(index);
+
+        for(let i = index; i < this.#_size-1; i++){
+            this.#_arr.set(i, this.#_arr.get(i+1));
+        }
+
+        this.#_arr.set(this.#_size-1, undefined);
+
+        this.#_size--;
+
+        return removedItem;
+    }
+
+    clear(){
+        for(let i = 0; i < this.#_size; i++){
+            this.#_arr.set(i, undefined);
+        }
+        this.#_size = 0;
+    }
 }
