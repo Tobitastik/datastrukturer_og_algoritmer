d68c825eda459ad789708a823c2c81c897c7ea73
Tobias <144216903+Tobitastik@users.noreply.github.com>
2025-11-26 16:46:17 +0100

    exchange sort

--- Algoritme-portfolio/exchangesort/exhangesort.js
+++ Algoritme-portfolio/exchangesort/exhangesort.js
@@ -1,3 +1,25 @@
-export function exhangesort(){
+export function exchangeSort(arr) {
     
+    const arrBefore = [...arr];
+    for (let i = 0; i < arr.length - 1; i++) {
+        console.log("Outer loop at index:", i, "Value:", arr[i]);
+
+        for (let j = i + 1; j < arr.length; j++) {
+            console.log(`   Comparing ${arr[i]} and ${arr[j]}`);
+
+            if (arr[j] < arr[i]) {
+                console.log(`   Swapping ${arr[i]} and ${arr[j]}`);
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+
+    console.log("Before sort:", arrBefore);
+    console.log("Final sorted list:", arr);
 }
+
+const arr = [5, 8, 2, 1, 0, 4, 3, 9, 7, 6];
+
+exchangeSort(arr);
