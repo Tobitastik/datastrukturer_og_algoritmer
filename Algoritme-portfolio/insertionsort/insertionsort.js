00995190a34b6f1a167a791c1e2445e4c4d2a5cb
Tobias <144216903+Tobitastik@users.noreply.github.com>
2025-11-26 12:42:14 +0100

    insertSort began

--- Algoritme-portfolio/insertionsort/insertionsort.js
+++ Algoritme-portfolio/insertionsort/insertionsort.js
@@ -1,3 +1,18 @@
 export function insertSort(arr){
     const list = [5, 8, 2, 1, 0, 4, 3, 9, 7, 6];
+    let i = 1;
+    let key = list[i]
+    let j = i - 1;
+
+    while(j >= 0 && list[j] > key){
+        console.log(`Shifting ${list[j]} to the right`);
+        list[j + 1] = list[j];
+        j--;
+    }
+
+    list[j + 1] = key;
+
+    console.log("List after inserting key:", list);
 }
+
+insertSort();
