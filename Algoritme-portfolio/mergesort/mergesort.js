c6a1597304be3b055dcabe3600f7ad034ca1b9c3
Tobias <144216903+Tobitastik@users.noreply.github.com>
2025-11-26 15:32:12 +0100

    mergesort

--- Algoritme-portfolio/mergesort/mergesort.js
+++ Algoritme-portfolio/mergesort/mergesort.js
@@ -1,11 +1,45 @@
 export function mergeSort(arr){
+    if (arr.length <= 1) {
+        return arr; 
+    }
+
     const mid = Math.floor(arr.length / 2);
     const left = arr.slice(0, mid);
     const right = arr.slice(mid);
 
-    console.log("Left: ", left);
-    console.log("Right: ", right);
+    console.log("Split:", mid);
+    console.log("Left:", left);
+    console.log("Right:", right);
 
+    const sortedLeft = mergeSort(left);
+    const sortedRight = mergeSort(right);
+
+    return merge(sortedLeft, sortedRight);
+}
+
+function merge(left, right){
+    const result = [];
+    let i = 0, j = 0;
+
+    while(i < left.length && j < right.length){
+        if(left[i] < right[j]){
+            result.push(left[i]);
+            i++;
+        } else {
+            result.push(right[j]);
+            j++;
+        }
+    }
+
+    while(i < left.length){
+        result.push(left[i++]);
+    }
+    while(j < right.length){
+        result.push(right[j++]);
+    }
+
+    console.log("Merging: ", left, "+", right, " Result:", result)
+    return result;
 }
 
 const arr = [5, 8, 2, 1, 0, 4, 3, 9, 7, 6];
