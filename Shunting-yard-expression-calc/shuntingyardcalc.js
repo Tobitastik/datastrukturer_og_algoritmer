17d9e7ab6d6a93636d1969dbb143f727e9a40643
Tobias <144216903+Tobitastik@users.noreply.github.com>
2025-11-27 13:09:43 +0100

    added case for operators

--- Shunting-yard-expression-calc/shuntingyardcalc.js
+++ Shunting-yard-expression-calc/shuntingyardcalc.js
@@ -31,11 +31,30 @@ function shuntingYardCalc(){
         return output;
     }
 
-    //If the token is a number, it'll be added to the output
+    
     const handleToken = (token) => {
         switch(true){
+            //If the token is a number, it'll be added to the output
             case !isNaN(parseFloat(token)):
                 output += ' ' + token;
+                break;
+            
+            //If the token is an operator. If it have higher prec, pop it, else push
+            case Object.keys(operators).includes(token):
+                const o1 = token;
+                let o2 = stack.at(-1);
+                
+                while(
+                    o2 !== undefined &&
+                    o2 !== '(' &&
+                    (operators[o2].prec > operators[o1].prec ||
+                        (operators[o2].prec === operators[o1].prec &&
+                            operators[o1].assoc === 'left'))
+                ) {
+                    output += ' ' + stack.pop();
+                    o2 = stack.at(-1);
+                }
+                stack.push(o1);
                 break;
         }
     }
