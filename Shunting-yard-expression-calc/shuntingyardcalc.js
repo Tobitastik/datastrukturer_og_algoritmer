6143fc4e8165e156f27dbcbda1632bfd78cb0761
Tobias <144216903+Tobitastik@users.noreply.github.com>
2025-11-27 14:12:04 +0100

    Only works with single digit numbers

--- Shunting-yard-expression-calc/shuntingyardcalc.js
+++ Shunting-yard-expression-calc/shuntingyardcalc.js
@@ -18,31 +18,41 @@ function shuntingYardCalc(){
         },
     };
 
+    const assert = (predicate) => {
+        if(predicate){
+            return;
+        }
+        throw new Error('Assertion failed, invalid token');
+    };
+
     const toRPN = (input) => {
+        const opSymbols = Object.keys(operators);
         const stack = [];
         let output = '';
 
-        for(let i of input){
-            if(i === '') continue;
-            
-            handleToken(i);
-        }
+    const peek = () => {
+        return stack.at(-1);
+    };
 
-        return output;
+    //If the token is a number, it'll be added to the output
+    const addToOutput = (token) => {
+        output += ' ' + token;
+    };
+
+    const handlePop = () => {
+        return stack.pop();
     }
 
-    
     const handleToken = (token) => {
         switch(true){
-            //If the token is a number, it'll be added to the output
             case !isNaN(parseFloat(token)):
-                output += ' ' + token;
+                addToOutput(token);
                 break;
-            
+
             //If the token is an operator. If it have higher prec, pop it, else push
-            case Object.keys(operators).includes(token):
+            case opSymbols.includes(token):
                 const o1 = token;
-                let o2 = stack.at(-1);
+                let o2 = peek();
                 
                 while(
                     o2 !== undefined &&
@@ -51,12 +61,46 @@ function shuntingYardCalc(){
                         (operators[o2].prec === operators[o1].prec &&
                             operators[o1].assoc === 'left'))
                 ) {
-                    output += ' ' + stack.pop();
-                    o2 = stack.at(-1);
+                    addToOutput(handlePop());
+                    o2 = peek();
                 }
                 stack.push(o1);
                 break;
+            
+            case token === '(':
+                stack.push(token);
+                break;
+            
+            case token === ')':
+                let topOfStack = peek();
+                while (topOfStack !== '('){
+                    assert(stack.length !== 0);
+                    addToOutput(handlePop());
+                    topOfStack = peek();
+                }
+                assert(stack.at(-1) === '(');
+                handlePop();
+                break;
+            default:
+                throw new Error(`Invalid token: ${token}`);   
         }
+    };
+
+    for(let i of input){
+        if(i === ' ') continue;
+        handleToken(i);
     }
 
+    while(stack.length !== 0){
+        assert(peek() !== '(');
+        addToOutput(stack.pop());
+    }
+    return output;
+    };
+    
+    return{toRPN};    
 }
+
+const calc = shuntingYardCalc();
+
+console.log(calc.toRPN("123+2")); 
